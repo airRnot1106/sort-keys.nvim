3bc0812ba41f0fc6a5ad6a60d9de80e3153a5b36
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-25 22:48:40 +0900

    chore: add flake.nix

---
+++ default.nix
@@ -0,0 +1,16 @@
+{
+  lib,
+  buildVimPlugin,
+}:
+buildVimPlugin {
+  pname = "sort-keys.nvim";
+  version = "0.0.0";
+
+  src = ./.;
+
+  meta = {
+    description = "Sort object/table keys using tree-sitter, similar to the built-in sort command";
+    homepage = "https://github.com/airRnot1106/sort-keys.nvim";
+    license = lib.licenses.mit;
+  };
+}
